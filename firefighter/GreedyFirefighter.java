05aa9d406bd090179481a77197f76d84214f5c86
ethankelly <72452934+ethankelly@users.noreply.github.com>
2020-10-29 16:53:03 +0000

    Update GreedyFirefighter.java

--- firefighter/GreedyFirefighter.java
+++ firefighter/GreedyFirefighter.java
@@ -1,32 +1,26 @@
-
 package ethankelly.graphs.fire;
 
 /*****
- *
- * @author Ethan Kelly
- * e.kelly.1@research.gla.ac.uk
- *
- *****/
-
-//NB THIS CODE IS NOT YET FUNCTIONAL
+*
+* Firefighter defence uses Greedy algorithm (1/2 approx. for optimal solution)
+*
+* @author Ethan Kelly
+* e.kelly.1@research.gla.ac.uk
+*
+*****/
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Random;
 
 public class GreedyFirefighter extends Graph {
 
-	private static int[] defence;
-	private static int[] burning;
-	private static int[] burned;
-
+	public static int[][] state;
 
 	public GreedyFirefighter(int numVertices) {
 		super(numVertices);
 	}
 
-// Random vertex selection for outbreak of fire
+	// Choose a random vertex selection for outbreak of fire
 	public static int outbreak(int numVertices) {
 		int start = new Random().nextInt(numVertices);
 
@@ -34,152 +28,133 @@ public class GreedyFirefighter extends Graph {
 		return start;
 	}
 
-// Construct the union of two arrays
-	public static int[] unionArrays(int[]... arrays) {
-        int maxSize = 0;
-        int counter = 0;
+// Initialise 2D array for state
+	public static int[][] initialState(int numVertices, int start, int turnCount) {
+		int[][] state = new int[numVertices][turnCount + 1];
 
-        for(int[] array : arrays) {
-        	maxSize += array.length;
-        }
-        int[] accumulator = new int[maxSize];
+		for (int n = 0; n <= turnCount; n++) {
+			state[start][n] = 2;
+		}
 
-        for(int[] array : arrays)
-            for(int i : array)
-                if(!isDuplicated(accumulator, counter, i))
-                    accumulator[counter++] = i;
+		// 0 -> open; 1 -> defended; 2 -> burning.
 
-        int[] result = new int[counter];
-        for(int i = 0; i < counter; i++) {
-        	result[i] = accumulator[i];
-        }
+		return state;
+	}
 
-        return result;
-    }
+// Determine which vertices burn
+	public static int[] burn(int[][] state, int turnCount) {
+		int[] burningVertices = new int[numVertices];
 
-    public static boolean isDuplicated(int[] array, int counter, int value) {
-    	boolean duplicate = false;
-    	
-        for(int i = 0; i < counter; i++) {
-        	if(array[i] == value) {
-        		duplicate = true;
-        	} else {
-        		duplicate = false;
-        	}
-        }
-		return duplicate;
-    }
+		if (turnCount % 2 != 0) {
+			// TODO Make this an exception!
+			System.out.println("Not an even numbered turn, cannot burn adjacent vertices");
+		} else
+			for (int i = 0; i < numVertices; i++) {
+				for (int j = 0; j < numVertices; j++) {
+					if (state[i][turnCount - 1] == 2) {
+						burningVertices[i] = 1;
+						if (state[j][turnCount - 1] == 0 && getEdge(i, j) == true) {
+							burningVertices[j] = 1;
+						}
+					}
+				}
+			}
+		int[] previouslyBurning = new int[numVertices];
+		for (int k = 0; k < numVertices; k++) {
+			if (state[k][turnCount - 1] == 2) {
+				previouslyBurning[k] = 1;
+			}
+		}
 
-// Find the neighbours of a vertex, output as boolean[]
-	public static boolean[] getNeighbours(int vertex) {
+		if (Arrays.equals(burningVertices, previouslyBurning)) {
+			Arrays.fill(burningVertices, -1);
+		}
 
-		boolean[] neighbours = new boolean[numVertices];
+		return burningVertices;
+
+	}
+
+// Update the state of the graph given what is currently burning
+	public static int[][] updateStateBurning(int[][] state, int[] toBurn, int turnCount) {
+		int[][] updatedBurning = new int[numVertices][turnCount + 1];
 		for (int i = 0; i < numVertices; i++) {
-			// Return true if i is neighbours with vertex,
-			// False if not
-			if (getEdge(i, vertex) == true) {
-				neighbours[i] = true;
-			} else {
-				neighbours[i] = false;
+			for (int j = 0; j < turnCount; j++) {
+				updatedBurning[i][j] = state[i][j];
+				updatedBurning[i][j + 1] = state[i][j];
 			}
 		}
 
-		return neighbours;
-	}
-	
-// What has been burned previously?
-	public static int[] previouslyBurned(int start, int turnCount) {
-			int[] burned = new int[numVertices];
-			Arrays.fill(burned, numVertices + 1);
-			burned[0] = start;
-			
-			int counter = 0;
-			
-			if (turnCount == 0) {
-				counter = 1;
-			} else if (turnCount > 0) {
-				for (int j : unionArrays(currentlyBurning(start, turnCount-1, previouslyBurned(start, turnCount-1), defence), previouslyBurned(start, turnCount-1))) {
-					if (j<numVertices) {
-					burned[j] = j;
-					counter++;
-
-					int[] shorterBurned = Arrays.copyOfRange(burned, 0, counter);
-					System.out.println("Burned: " + Arrays.toString(shorterBurned));
-					}
-				}
-			}
-			
-			return burned;
-	}
-	
-// What is currently burning?
-	@SuppressWarnings("unlikely-arg-type")
-	public static int[] currentlyBurning(int start, int turnCount, int[] burned, int[] defence) {
-		
-
-		int[] onFire = new int[numVertices];
-		Arrays.fill(onFire, numVertices+1);
-		int counter = 0;
-					
-		List<int[]> listDefence = Arrays.asList(defence);
-		
-			// If this is t=0, only vertex on fire is start
-			if (turnCount == 0) {
-				onFire[0] = start;
-				onFire = Arrays.copyOfRange(onFire, 0, turnCount+1);
-				counter = 1;
-			// t=1 or greater	
-			} else if (turnCount>0) {
-				for (int i = 0; i < numVertices; i++) {
-				// for each i in set of vertices and
-				// for each j on fire, are i and j neighbours?
-					for (int j : burning) {
-						if (j > numVertices) {
-							break;
-						} else if (getEdge(i, j)==true && listDefence.contains(i) == false && i !=j) {
-							onFire[counter] = i;
-							counter++;
-						}
-					}
-				}
-			}
-			
-		int[] shortOnFire = Arrays.stream(Arrays.copyOfRange(onFire, 0, counter)).distinct().toArray();
-		System.out.println("On fire: " + Arrays.toString(shortOnFire));
-		return onFire;
-	}
-	
-// What do we defend?
-	public static int[] defenceStrategy(int start, int turnCount, int[] defence, int[] burning, int[] burned, boolean[][] adjMatrix) {
-		
-		int[] weights = weightsArray(adjMatrix);
-		Integer[] weightsInOrder = new Integer[weights.length];
-		
-		for (int n=0; n < weights.length; n++) {
-			weightsInOrder[n] = weights[n];
-		}
-		
-		Arrays.sort(weightsInOrder, Collections.reverseOrder());
-		
-		for (int i : defence) {
-			for (int j : burned) {
-				for (int k : burning) {
-					for (int n = 0; n < numVertices; n++ ) {
-					
-						int candidate = weightsInOrder[n];
-				
-						if (i != weights[candidate] && j != weights[candidate] && k != weights[candidate]) {
-							defence[turnCount] = weights[candidate];
-
-						}
+		for (int i = 0; i < numVertices; i++) {
+			for (int j = 0; j < numVertices; j++) {
+				// If something is previously burned or defended, ensure all following
+				// incidences also that state
+				if (toBurn[i] == 1) {
+					if (getEdge(i, j) == true) {
+						updatedBurning[j][turnCount] = 2;
 					}
 				}
 			}
 		}
-		int[] shortDefence = Arrays.copyOfRange(defence, 0, turnCount+1);
-		System.out.println("Defence: " + Arrays.toString(shortDefence));
-		return defence;
-	}		
+
+		// System.out.print(Arrays.deepToString(updatedBurning));
+		return updatedBurning;
+	}
+
+// Choose which vertex to defend each odd turnCount
+	public static int defence(int[][] state, int turnCount) {
+		int defendVertex = 0;
+		int[] possibleWeights = weightsArray(adjMatrix);
+
+		if (turnCount % 2 == 0) {
+			// TODO Make this an exception!
+			System.out.println("Not an odd numbered turn, cannot defend");
+		} else
+			for (int i = 0; i < numVertices; i++) {
+				if (state[i][turnCount - 1] != 0) {
+					possibleWeights[i] = 0;
+				}
+				defendVertex = largestWeight(possibleWeights);
+			}
+		for (int j = 0; j < numVertices; j++) {
+			if (getEdge(defendVertex, j) == true) {
+				removeEdge(defendVertex, j);
+			}
+		}
+		if (possibleWeights[defendVertex] == 0) {
+			defendVertex = numVertices + 1;
+		} else {
+			System.out.println("Defending heaviest possible vertex: " + defendVertex);
+		}
+		return defendVertex;
+	}
+
+// Update the state of the graph given defence information
+	public static int[][] updateStateDefence(int[][] state, int defend, int turnCount) {
+
+		int[][] updatedDefence = new int[numVertices][turnCount + 1];
+		for (int i = 0; i < numVertices; i++) {
+			for (int j = 0; j < turnCount; j++) {
+				updatedDefence[i][j] = state[i][j];
+				updatedDefence[i][j + 1] = state[i][j];
+			}
+		}
+
+		for (int k = turnCount; k <= turnCount; k++) {
+			updatedDefence[defend][k] = 1;
+			// Only update what has been defended
+		}
+		// System.out.println(Arrays.deepToString(updatedState));
+		return updatedDefence;
+
+	}
+
+	public static void printMatrix(int[][] matrix) {
+		// Loop through all rows
+		for (int[] row : matrix)
+
+			// convert each row to string and then printing in a separate line
+			System.out.println(Arrays.toString(row));
+	}
 
 // Main method	
 	public static void main(String[] args) {
@@ -198,19 +173,50 @@ public class GreedyFirefighter extends Graph {
 		System.out.println("Initial graph:");
 		System.out.print(g.toString());
 
+		int turnCount = 0;
 		int start = outbreak(numVertices);
-		
-		int[] defence = new int[numVertices];
-		Arrays.fill(defence, numVertices + 1);
-		
+		int[][] state = initialState(numVertices, start, turnCount);
+		System.out.println("Initial state of play: ");
+		printMatrix(state);
+		turnCount++;
 
-		for (int i = 0; i < 2; i++) {
-			System.out.println("---- Round: " + i + " ----");
-			
-			burned = previouslyBurned(start, i);
-			burning = currentlyBurning(start, i, defence, burned);
-			defence = defenceStrategy(start, i, defence, burning, burned, adjMatrix);
+		for (int c = turnCount; c < numVertices * 10; c++) {
+			System.out.println("---- Turn Count: " + c + " ----");
+			if (c % 2 == 1) {
+				int defend = defence(state, c);
+				if (defend > numVertices) {
+					System.out.println("No defensive moves can be made; game over.");
+					break;
+				} else {
+					int[][] newState = updateStateDefence(state, defend, c);
+					System.out.println("After defence: ");
+					printMatrix(newState);
+					state = newState;
+				}
+
+			} else {
+				int[] toBurn = burn(state, c);
+				int[] comparator = new int[numVertices];
+				Arrays.fill(comparator, -1);
+				if (Arrays.equals(toBurn, comparator)) {
+					System.out.println("No vertices can be burned; game over.");
+					break;
+				} else {
+
+					System.out.print("Burning: ");
+					for (int i = 0; i < numVertices; i++) {
+						if (toBurn[i] == 1) {
+							System.out.print(i + " ");
+						}
+					}
+					System.out.println();
+					int[][] newState = updateStateBurning(state, toBurn, c);
+					System.out.println("After burning: ");
+					printMatrix(newState);
+					state = newState;
+				}
+
+			}
 		}
 	}
 }
-

2d7fe89ee33cbd04d510a9aa28421f4b2f3cb89c
ethankelly <72452934+ethankelly@users.noreply.github.com>
2020-11-12 11:55:09 +0000

    Added latest versions and backed up previous

--- firefighter/src/io/github/ethankelly/NaiveOptimalFirefighter.java
+++ firefighter/src/io/github/ethankelly/NaiveOptimalFirefighter.java
@@ -1,6 +1,6 @@
 package io.github.ethankelly;
 
-import java.util.ArrayList;
+
 import java.util.Arrays;
 
 /**
@@ -21,103 +21,113 @@ public class NaiveOptimalFirefighter extends Graph {
         for (int i = 0; i < numVertices; ++i) a[i] = i;
         return a;
     }
+
+    // Calculate factorial of an integer input
     public static int factorial(int input) {
         if (input <= 2) {
             return input;
         }
         return input * factorial(input - 1);
     }
-
+    
     /**
-     * @param perm  permutation of the open vertices
-     * @return      optimal defence strategy from the combinations
+     * Determine optimal defence strategy
+     *
+     * @param perm permutation of the open vertices
+     * @return optimal defence strategy from the combinations
      */
-    public static int[] defence(Permutations perm) {
-        int[][] permMatrix = perm.asMatrix();
-        int[] pOptimal = new int[numVertices];
-        for (int r = 0; r < factorial(numVertices); r++){
-            for (int c = 0; c < numVertices; c++) {
-                for (int i = 0; i < numVertices; i++) {
-                    if (getEdge(permMatrix[r][c], i)) {
-                        removeEdge(permMatrix[r][c], i);
-                    }
-                }
+    public static int[] optimalDefence(int[][] perm, int[][] state) {
 
-            }
+        /*
+         * Cycle through defence strategies
+         * discount any that aren't possible
+         * find end turn count for each and how many vertices burned in each (did we play all defences?)
+         * use this info to determine the optimal strategy of all permutations
+         */
+        int[] pOptimal = new int[numVertices];
+        for (int i = 0; i < perm[0].length; i++) {
+            defence(perm[i], i, state);
         }
 
         return pOptimal;
     }
 
-    private static class Permutations {
-        public static int[] input;
-        public final int numPerm;
-        public final ArrayList<int[]> newList;
-
-        public Permutations(int[] openVertices) {
-            input = openVertices;
-            numPerm = factorial(input.length);
-            newList = new ArrayList<>(numPerm);
+    public static boolean canDefend(int[] strategy, int permNumber, int index, int[][] state) {
+        boolean defencePossible = false;
+        if (state[permNumber][index] != strategy[index]) {
+            defencePossible = true;
         }
-
-        public void permute(int[] openVertices) {
-            permutare(openVertices, 0, openVertices.length);
+        return defencePossible;
+    }
+    
+    public static int[][] defence(int[] strategy, int permNumber, int[][] state) {
+        /*
+         * Given a strategy (and which row it is in permutation matrix)
+         * Play defence at position i (starting at 0)
+         * burn all open neighbours of the outbreak
+         * i++
+         * possible to defend vertex at position i?
+         * Yes: defend, i++, burn relevant vertices, possible to defend at i?
+         * No: "Impossible defence strategy," break
+         * How many of the vertices was it necessary to defend, if we defended all? When was the fire contained?
+         *
+         */
+        for (int i = 1; i < strategy.length; i++) {
+            if (canDefend(strategy, permNumber, i, state)) {
+                state = updateStateBurning(updateStateDefence(state, strategy[i-1], i), burn(state, i+1), i+1);
+            } else {
+                System.out.println(" Permutation " + Arrays.toString(strategy) + " is not a valid defence strategy");
+                break;
+            }
         }
+        return state;
+    }
+    
+    /**
+     * Generate matrix populated with all permutations of openVertices
+     *
+     * @param   openVertices int array of all vertices neither burning nor defended
+     * @return  2d array populated with all permutations of openVertices
+     */
+    public static int[][] permute(int[] openVertices) {
+        return permuteMethod(openVertices, 0, openVertices.length);
+    }
+    // Implement Heap's algorithm to find all permutations of given array of open vertices
+    private static int[][] permuteMethod(int[] array, int start, int end) {
+        int[][] permutation = new int[factorial(array.length)][array.length];
 
-        private void permutare(int[] array, int start, int end) {
-            newList.add(saveArray(      array));
-
-            if (start < end) {
-                int i, j;
-                for (i = end - 2; i >= start; i--) {
-                    for (j = i + 1; j < end; j++) {
-                        swap(array, i, j);
-                        permutare(array, i + 1, end);
-                    }
-                    rotateLeft(array, i, end);
+        if (start < end) {
+            int i, j;
+            for (i = end - 2; i >= start; i--) {
+                for (j = i + 1; j < end; j++) {
+                    swap(array, i, j);
+                    permuteMethod(array, i + 1, end);
+                    //System.out.println(Arrays.toString(array));
                 }
+                rotateLeft(array, i, end);
+                permutation[i] = array;
             }
         }
-
-        private int[] saveArray(int[] array) {
-            int[] newArray = new int[array.length];
-
-            System.arraycopy(array, 0, newArray, 0, array.length);
-            return newArray;
-        }
-
-        public void print() {
-            int[] array;
-            for (int[] ints : newList) {
-                array = ints;
-                System.out.println(Arrays.toString(array));
-            }
-        }
-
-        private void swap(int[] array, int i, int j) {
-            int t;
-            t = array[i];
-            array[i] = array[j];
-            array[j] = t;
-        }
-
-        private void rotateLeft(int[] array, int start, int end) {
-            int tmp = array[start];
-            if (end - 1 - start >= 0) System.arraycopy(array, start + 1, array, start, end - 1 - start);
-            array[end - 1] = tmp;
-        }
-
-
-        public int[][] asMatrix() {
-            int[][] permAsArray = new int[factorial(numVertices)][numVertices];
-
-
-            return permAsArray;
-        }
+        return permutation;
     }
 
+    // Swaps two array elements
+    private static void swap(int[] array, int i, int j) {
+        int t;
+        t = array[i];
+        array[i] = array[j];
+        array[j] = t;
+    }
+    // Rotates the array
+    private static void rotateLeft(int[] array, int start, int end) {
+        int tmp = array[start];
+        if (end - 1 - start >= 0) System.arraycopy(array, start + 1, array, start, end - 1 - start);
+        array[end - 1] = tmp;
+    }
+
+
     /**
-     * @param args the command line arguments
+     *
      */
     public static void main(String[] args) {
         // Define the graph
@@ -145,10 +155,10 @@ public class NaiveOptimalFirefighter extends Graph {
         for (int i = 0, k = 0; i < numVertices; i++) if (i != start) openVertices[k++] = vertices[i];
 
         // Give all permutations of the open vertices
-        Permutations newPerm = new Permutations(openVertices);
-        newPerm.permute(openVertices);
-        newPerm.print();
+        int[][] newPerm = permute(openVertices);
+        printMatrix(newPerm);
 
-        defence(newPerm);
+        int[] defend = optimalDefence(newPerm, state);
+        System.out.println(Arrays.toString(defend));
     }
 }
--- firefighter/version-history/12-11-20/src/io/github/ethankelly/Graph.java
+++ firefighter/version-history/12-11-20/src/io/github/ethankelly/Graph.java
@@ -40,6 +40,44 @@ public class Graph {
         return adjMatrix[i][j] || adjMatrix[j][i];
     }
 
+/* Construct the union of two arrays
+	public static int[] unionArrays(int[]... arrays) {
+		int maxSize = 0;
+		int counter = 0;
+
+		for (int[] array : arrays) {
+			maxSize += array.length;
+		}
+		int[] accumulator = new int[maxSize];
+
+		for (int[] array : arrays)
+			for (int i : array)
+				if (!isDuplicated(accumulator, counter, i))
+					accumulator[counter++] = i;
+
+		int[] result = new int[counter];
+		for (int i = 0; i < counter; i++) {
+			result[i] = accumulator[i];
+		}
+
+		return result;
+	}
+
+	public static boolean isDuplicated(int[] array, int counter, int value) {
+
+		boolean duplicate = false;
+
+		for (int i = 0; i < counter; i++) {
+			if (array[i] == value) {
+				duplicate = true;
+			} else {
+				duplicate = false;
+			}
+		}
+		return duplicate;
+	}
+*/
+
     // Determine outbreak of fire on graph (choosing random vertex)
     public static int outbreak(int numVertices) {
         int start = new Random().nextInt(numVertices);

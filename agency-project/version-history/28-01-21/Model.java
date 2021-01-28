79f6c036fc53cee1daa9173882a0bf3fa6ba6a71
Ethan Kelly <72452934+ethankelly@users.noreply.github.com>
2021-01-28 18:35:06 +0000

    Update Model.java

--- agency-project/version-history/28-01-21/Model.java
+++ agency-project/version-history/28-01-21/Model.java
@@ -1,54 +1,15 @@
 package io.github.ethankelly;
 
-import java.util.Arrays;
-
 /**
- * The agency-based model of this project is driven by this main class. The methods here
- * are used to pass probabilities which dictate infection dynamics, depending on the
- * particular context we want to consider.
+ * The agency-based model of this project is driven by this main class.
  *
  * @author Ethan Kelly
  */
 public class Model {
 
-    // Returns an array of any vertices currently infected (on fire)
-    public static int[] getFires() {
-        int[][] state = ModelState.getGraphState();
-        int vertices = state.length;
-        int currentRound = state[0].length;
-
-        int[] vertexSet = new int[vertices];
-        for (int i = 0; i < vertices; ++i) vertexSet[i] = i;
-
-        int[] onFire = new int[vertices - 1];
-        int k = 0;
-        for (int i = 0; i < vertices; i++) {
-            if (state[i][currentRound-1] == 2) {
-                onFire[k++] = vertexSet[i];
-            }
-        }
-        return Arrays.copyOf(onFire, k);
-    }
-
-    public static int[] getDefended() {
-        int[][] state = ModelState.getGraphState();
-        int vertices = state.length;
-        int currentRound = state[0].length;
-
-        int[] vertexSet = new int[vertices];
-        for (int i = 0; i < vertices; ++i) vertexSet[i] = i;
-
-        int[] defended = new int[vertices - 1];
-        int k = 0;
-        for (int i = 0; i < vertices; i++) {
-            if (state[i][currentRound-1] == 1) {
-                defended[k++] = vertexSet[i];
-            }
-        }
-        return Arrays.copyOf(defended, k);
-    }
-
-    // Tests graph initialisation, model state and agent object creation
+    /**
+     * Tests graph initialisation, model state and agent object creation
+     */
     private static void runTest() {
         int numVertices = 9;
         Graph g = new Graph(numVertices);
@@ -65,7 +26,7 @@ public class Model {
 
         System.out.println("   *******    ");
 
-        System.out.println(Arrays.toString(getFires()));
+//        System.out.println(Arrays.toString(getFires()));
 
         for (int i = 0; i < numVertices; i++) {
             Agent a = new Agent(i);
